79d64238243ccd2d59c2c043fb440fb7877a6f5c
DESKTOP-72CDNFK\quang <quangmaster93@gmail.com>
2018-06-28 15:58:42 +0700

    add Header for map vs schedule

--- app/screen/ScreenMap.js
+++ app/screen/ScreenMap.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
-    StatusBar
+    StatusBar,
+    StyleSheet
 
 } from 'react-native';
-
+import HeaderTabMapVsSchedule from "../component/HeaderTabMapVsSchedule"
 export default class ScreenMap extends Component<any, any> {
     constructor(props: any) {
         super(props);
@@ -15,7 +16,14 @@ export default class ScreenMap extends Component<any, any> {
 
     }
     render() {
-        return <View>
+        return <View style={styles.container}>
+            <HeaderTabMapVsSchedule OpenMenu={() => this.props.navigation.openDrawer()}
+                OpenMap={() => this.props.navigation.navigate("ScreenMap")}
+                OpenSchedule={() => this.props.navigation.navigate("ScheduleTab")}
+                OpenNotification={() => this.props.navigation.navigate("ScreenNotification")}
+                notificationNumber={0}
+                isMap={true}
+            />
             <StatusBar translucent backgroundColor="#416af3"></StatusBar>
             <Text>
                 ScreenMap
@@ -23,3 +31,11 @@ export default class ScreenMap extends Component<any, any> {
         </View>
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: "#FFFFFF"
+    }
+})
